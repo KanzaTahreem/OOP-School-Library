a4f98263a4fbf958ecacefe8a153f4a2fc4f8a26
KanzaTahreem <tehreemmsk411@gmail.com>
2023-06-15 00:22:54 +0500

    Use require_relative for spec files

--- src/spec/classroom_spec.rb
+++ src/spec/classroom_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require_relative './spec_helper'
 
 describe Classroom do
   describe '>> new classroom' do
--- src/spec/student_spec.rb
+++ src/spec/student_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require_relative './spec_helper'
 
 describe Student do
   before :each do
